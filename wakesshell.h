18c73adee28fd0de49e4bb6c13f7ef716234edd7
ayahhmeed <ayahhmeed@gmail.com>
2023-11-08 13:35:46 -0800

    headerfile

---
+++ wakesshell.h
@@ -0,0 +1,13 @@
+#ifndef WAKES_SHELL_H
+#define WAKES_SHELL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+
+void wakesprint (const char* str)
+
+#endif /* WAKES_SHELL_H */
