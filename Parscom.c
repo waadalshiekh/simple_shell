e3e99f8ebabd86fb6412db5e2f57c980356749c1
Waad Alshiekh <waad9726@gmail.com>
2023-11-11 09:39:39 -0800

    e

--- Parscom.c
+++ Parscom.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-int parscom(int input, char *comm[])
-    for (int i = 0; comm[i] != NULL; i++) //input here from user
+int parscom(int input, char *comm[]) //input here from user
+    for (int i = 0; comm[i] != NULL; i++)
         //here we should put the result
     return = result;
 }
